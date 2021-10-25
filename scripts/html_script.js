b34fd93e67e2195494d1426109e939720b46e18c
Mattias Kind <mattias.kind@gmail.com>
2021-10-25 14:35:48 +0200

    Skriptet använder innerText istället för innerHTML

--- scripts/html_script.js
+++ scripts/html_script.js
@@ -4,19 +4,12 @@ const button = document.querySelector('.btn-message');
 const userMessage = document.querySelector('#message');
 const formMsgContainer = document.querySelector('.form-msg-container');
 
-// Funktionen sanitizeString är hämtad från stackoverflow.
-// Jag behövde ha ett regex som tar bort otillåtna tecken.
-const sanitizeString = function (str) {
-  str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim, '');
-  return str.trim();
-};
-
 // Funktionen nedan skickas vidare till event-hanteraren och körs när besökaren klickar på knappen i formuläret.
 const displayMessage = function (e) {
   e.preventDefault();
 
   // Ta bort eventuellt tidigare meddelande
-  formMsgContainer.innerHTML = '';
+  formMsgContainer.innerText = '';
 
   // Visa rutan för meddelanden om den är gömd
   if (formMsgContainer.classList.contains('hidden'))
@@ -24,19 +17,11 @@ const displayMessage = function (e) {
 
   // Kontrollera om input-fältet tomt eller enbart innehåller blanksteg
   if (!userMessage.value.trim()) {
-    const html = `<p>Du måste ange ett meddelande.</p>`;
-    formMsgContainer.insertAdjacentHTML('afterbegin', html);
+    const text = `Du måste ange ett meddelande.`;
+    formMsgContainer.innerText = 'Du måste ange ett meddelande';
     return;
   }
-
-  // Ta bort eventuella förbjudna tecken
-  const sanitizedMessage = sanitizeString(userMessage.value);
-  // Skapa html
-  const html = `
-  <p>Du skrev: ${sanitizedMessage}</p>
-  `;
-  // Visa html och meddelandet på sidan
-  formMsgContainer.insertAdjacentHTML('afterbegin', html);
+  formMsgContainer.innerText = userMessage.value;
 };
 
 // Lägger till event-listener på formulär-knappen
